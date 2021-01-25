54a8b2eb764eb1e6ef91859eaf1c7549ce160ff3
Lucas Rocha de Almeida <lralmeida@PREFEITURA.VGP>
2021-01-25 15:46:32 -0300

    subindo o projeto

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+// função add guia
+function duplicarCampos(){
+    var clone = document.getElementById('origem').cloneNode(true);
+    var destino = document.getElementById('destino');
+    destino.appendChild (clone);
+    
+    var camposClonados = clone.getElementsByTagName('input');
+    
+    for(i=0; i<camposClonados.length;i++){
+        camposClonados[i].value = '';
+    }
+    
+    
+    
+}
+
+function removerCampos(event){
+    const span = event.currentTarget
+
+    const fieldsContainer = document.querySelectorAll('destino')
+
+    
+    //deletar o campo
+    span.parentNode.remove()
+}
